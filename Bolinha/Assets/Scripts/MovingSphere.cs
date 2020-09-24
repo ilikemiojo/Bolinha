336d1361a5bfa04c35c1466e344f1f153cc77590
ilikemiojo <anwoiciruam@gmail.com>
2020-09-24 12:30:26 -0300

    Completed Part 4: Orbit Camera

--- Bolinha/Assets/Scripts/MovingSphere.cs
+++ Bolinha/Assets/Scripts/MovingSphere.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
+    [SerializeField]
+    Transform playerInputSpace = default;
+    
     [SerializeField, Range(0f, 100f)]
     float maxSpeed = 10f;
 
@@ -67,8 +70,20 @@ public class MovingSphere : MonoBehaviour
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
         
-        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        if (playerInputSpace)
+        {
+            Vector3 forward = playerInputSpace.forward;
+            forward.y = 0f;
+            forward.Normalize();
 
+            Vector3 right = playerInputSpace.right;
+            right.y = 0f;
+            right.Normalize();
+
+            desiredVelocity = (forward * playerInput.y + right * playerInput.x) * maxSpeed;
+        }
+        else { desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed; }
+        
         desiredJump |= Input.GetButtonDown("Jump");
     }
 
