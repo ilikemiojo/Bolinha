0fb67620711c72a55caa961d24308687213fc713
ilikemiojo <anwoiciruam@gmail.com>
2020-11-14 00:27:20 -0300

    Update

---
+++ Bolinha/Assets/Scripts/Environment/ItemCollector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemCollector : MonoBehaviour
+{
+    public static int score = 0;
+
+    public GameObject item;
+    public TextMeshProUGUI scoreText;
+
+    public void Collect()
+    {
+        score++;
+        scoreText.text = "SCORE: " + score.ToString(); 
+        Destroy(item);
+    }
+}
